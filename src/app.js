63bba2f9679d248504fc6e89c9443d7453c6a6d6
cookie <cookie.chen@autodesk.com>
2018-04-24 07:07:39 +0800

    highlight plugin

--- src/app.js
+++ src/app.js
@@ -7,9 +7,28 @@ import Image from '@ckeditor/ckeditor5-image/src/image';
 import InsertImage from './InsertImage';
 import ImageCaption from '@ckeditor/ckeditor5-image/src/imagecaption';
 import Heading from '@ckeditor/ckeditor5-heading/src/heading';
+import Highlight from '@ckeditor/ckeditor5-highlight/src/highlight';
 
 ClassicEditor
   .create(document.querySelector('#editor'), {
+    highlight: {
+      options: [
+        {
+          model: 'greenMarker',
+          class: 'marker-green',
+          title: 'Green marker',
+          color: 'var(--ck-highlight-marker-green)',
+          type: 'marker'
+        },
+        {
+          model: 'redPen',
+          class: 'pen-red',
+          title: 'Red pen',
+          color: 'var(--ck-highlight-pen-red)',
+          type: 'pen'
+        }
+      ]
+    },
     heading: {
       options: [
         { model: 'paragraph', title: 'Paragraph', class: 'ck-heading_paragraph' },
@@ -17,8 +36,8 @@ ClassicEditor
         { model: 'heading2', view: 'h2', title: 'Heading 2', class: 'ck-heading_heading2' }
       ]
     },
-    plugins: [Heading, Essentials, Paragraph, Bold, Italic, Image, InsertImage, ImageCaption],
-    toolbar: ['heading', 'bold', 'italic', 'insertImage']
+    plugins: [Heading, Highlight, Essentials, Paragraph, Bold, Italic, Image, InsertImage, ImageCaption],
+    toolbar: ['heading', '|', 'highlight', 'bold', 'italic', 'insertImage']
   })
   .then(editor => {
     console.log('Editor was initialized', editor);

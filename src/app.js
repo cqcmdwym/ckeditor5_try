bdd2e6ef6feabce7e4e22ac4dad1a14bb8330785
cookie <cookie.chen@autodesk.com>
2018-04-24 06:59:19 +0800

    add heading

--- src/app.js
+++ src/app.js
@@ -6,11 +6,19 @@ import Italic from '@ckeditor/ckeditor5-basic-styles/src/italic';
 import Image from '@ckeditor/ckeditor5-image/src/image';
 import InsertImage from './InsertImage';
 import ImageCaption from '@ckeditor/ckeditor5-image/src/imagecaption';
+import Heading from '@ckeditor/ckeditor5-heading/src/heading';
 
 ClassicEditor
   .create(document.querySelector('#editor'), {
-    plugins: [Essentials, Paragraph, Bold, Italic, Image, InsertImage, ImageCaption],
-    toolbar: ['bold', 'italic','insertImage']
+    heading: {
+      options: [
+        { model: 'paragraph', title: 'Paragraph', class: 'ck-heading_paragraph' },
+        { model: 'heading1', view: 'h1', title: 'Heading 1', class: 'ck-heading_heading1' },
+        { model: 'heading2', view: 'h2', title: 'Heading 2', class: 'ck-heading_heading2' }
+      ]
+    },
+    plugins: [Heading, Essentials, Paragraph, Bold, Italic, Image, InsertImage, ImageCaption],
+    toolbar: ['heading', 'bold', 'italic', 'insertImage']
   })
   .then(editor => {
     console.log('Editor was initialized', editor);
